e3491aae0f3f0d08732f80bbd0f52d5ebb66a020
merzouk <merzouk@gmail.com>
2020-03-08 19:18:41 +0100

    update

--- src/main/java/org/com/CodingGamesApplication.java
+++ src/main/java/org/com/CodingGamesApplication.java
@@ -1,13 +1,14 @@
+
 package org.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CodingGamesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CodingGamesApplication.class, args);
-	}
-
+public class CodingGamesApplication
+{
+   public static void main( String[] args )
+   {
+      SpringApplication.run( CodingGamesApplication.class, args );
+   }
 }
